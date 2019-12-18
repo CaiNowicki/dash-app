0442a508dd09daf917d9ce5bcd1567f05ee2fd2c
dunkelweizen <cai.nowicki@gmail.com>
2019-12-17 23:22:34 -0500

    changed app description

--- pages/index.py
+++ pages/index.py
@@ -16,17 +16,12 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Your Value Proposition
+            ## Thanksgiving Quiz: Can We Guess Where You Are From Based on Thanksgiving Dinner?
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
 
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Take the Quiz Now!', color='primary'), href='/predictions')
     ],
     md=4,
 )
