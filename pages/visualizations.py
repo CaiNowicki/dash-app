eadc30597fdf29f35d7550a0546f4de44fe9106f
dunkelweizen <53185634+dunkelweizen@users.noreply.github.com>
2020-01-08 00:33:32 -0500

    added height and width to graph

--- pages/visualizations.py
+++ pages/visualizations.py
@@ -9,18 +9,18 @@ df = pd.read_csv('assets/wrangled_data.csv')
 
 #create plotly express visualizations
 figure1 = px.density_heatmap(df, x='Mac_and_Cheese', y='Type_of_Thanksgiving', hover_data=df.columns,
-                             title='Mac and Cheese Heatmap')
+                             title='Mac and Cheese Heatmap', width=400, height=400)
 figure1.update_xaxes(title_text='Was Mac and Cheese Served?', automargin=True)
 figure1.update_yaxes(title_text='Thanksgiving Regional Type', automargin=True)
 
 figure2 = px.density_heatmap(df, x='Mac_and_Cheese', y='Type_of_Thanksgiving', hover_data=df.columns,
-                             histnorm='percent', title='Mac and Cheese Percentage Heatmap')
+                             histnorm='percent', title='Mac and Cheese Percentage Heatmap', width=400, height=400)
 figure2.update_xaxes(title_text='Was Mac and Cheese Served?', automargin=True)
 figure2.update_yaxes(title_text='Thanksgiving Regional Type', automargin=True)
 
-figure3 = px.histogram(df, x='Main_Dish', y='Type_of_Thanksgiving', title='Main Dish Types')
+figure3 = px.histogram(df, x='Main_Dish', y='Type_of_Thanksgiving', title='Main Dish Types', width=400, height=400)
 figure3.update_xaxes(title_text='Main Dish', automargin=True)
-figure3.update_yaxes(title_text='Percentage of Total', automargin=True)
+figure3.update_yaxes(title_text='Total Count', automargin=True)
 
 column1 = dbc.Col(
     [
